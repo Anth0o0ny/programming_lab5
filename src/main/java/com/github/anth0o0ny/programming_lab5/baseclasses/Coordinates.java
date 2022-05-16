d56602b12005913071a29e9b70678e6286477168
Anth0o0ny <pushkin.a.s.03@mail.ru>
2022-05-16 22:58:56 +0300

    addition of Coordinates

--- src/main/java/com/github/anth0o0ny/programming_lab5/baseclasses/Coordinates.java
+++ src/main/java/com/github/anth0o0ny/programming_lab5/baseclasses/Coordinates.java
@@ -1,4 +1,29 @@
 package com.github.anth0o0ny.programming_lab5.baseclasses;
 
 public class Coordinates {
+    private Double x; //Максимальное значение поля: 398, Поле не может быть null
+    private Float y; //Поле не может быть null
+
+    public Coordinates(Double x, Float y){
+        setX(x);
+        setY(y);
+    }
+    public Double getX() {
+        return x;
+    }
+
+    public void setX(Double x) {
+        this.x = x;
+    }
+
+    public Float getY() {
+        return y;
+    }
+
+    public void setY(Float y) {
+        this.y = y;
+    }
+
+
+
 }
